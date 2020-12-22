902914989b2fd1f639c7c6320dda834073849a70
Atinder-Pal <atty.pal46@gmail.com>
2020-12-21 20:03:09 -0700

    Added highlight property to pipes to highlight thenm bird hits;

--- pipe.js
+++ pipe.js
@@ -5,18 +5,25 @@ class Pipe {
         this.x = width;
         this.pipeWidth = 20;
         this.speed = 2;
+        this.highlightPipe = false;
     }
 
     hits(bird){
         if(bird.y < this.top || bird.y > height - this.botton){
-            if(bird.x > this.x && bird.x < this.x + this.pipeWidth)
+            if(bird.x > this.x && bird.x < this.x + this.pipeWidth){
+                this.highlightPipe = true;
                return true; 
+            }                
         }
+        this.highlightPipe = false;;
         return false;
     }
 
     show(){
         fill(255);
+        if(this.highlightPipe){
+            fill(255,0,0);
+        }
         rect(this.x, 0, this.pipeWidth, this.top);
         rect(this.x, height -this.bottom, this.pipeWidth, this.bottom);
     }
