a744613c890116d1fbd066a20e03ce01e6087588
Atinder-Pal <atty.pal46@gmail.com>
2020-12-21 18:37:07 -0700

    Added code to keep the bird within canvas;

--- bird.js
+++ bird.js
@@ -2,10 +2,26 @@ class Bird {
     constructor(){
         this.y = height/2;
         this.x = 100;
+        this.gravity = 1;
+        this.velocity =0;
     }
 
     show(){
         fill(255);
         ellipse(this.x, this.y, 16, 16);
+    }
+
+    update(){
+        this.velocity += this.gravity;
+        this.y += this.velocity;
+
+        if(this.y > height){
+            this.y = height;
+            this.velocity = 0;
+        }
+        if(this.y < 0){
+            this.y = 0;
+            this.velocity = 0;
+        }
     }
 }
--- sketch.js
+++ sketch.js
@@ -7,5 +7,7 @@ function setup() {
 
 function draw() {
   background(0);
+
+  bird.update();
   bird.show();
 }
